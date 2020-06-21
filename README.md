9e033ed965f41c9ffdc4dab1f2be8465d72c12f4
Rajesh Kumar <rkumar@groupon.com>
2020-06-21 20:19:51 +0100

    api code and requirements file udpated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 This is a project for ADN Gauge.
+
+#### install all the dependencies
+`pip install -r requirements.txt`
+
+#### run migrate to setup the database
+`python manage.py migrate`
+
+#### start the server
+`python manage.py runserver`
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/login', TemplateView.as_view(template_name='login.html'), name='login'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('live-gauge', TemplateView.as_view(template_name='livegauge.html'), name='livegauge'),
 ]
